c885df26bb7e7ac5f49c92281d777cb9b1b90325
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-14 01:03:52 +0300

    st:navand paragraph styling - optimize paragraph styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FormComponent } from './form/form.component';
 import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FormComponent,
-    MenuComponent
+    MenuComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
