a6ac0fc863288cd306d827545e418e44947524eb
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-14 00:46:56 +0300

    st:paragraph-styling - optimizes paragraph styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FormComponent } from './form/form.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    FormComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
