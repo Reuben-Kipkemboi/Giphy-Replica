faaffb62922e2441a19ebce87219f7e65887dfb6
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-15 09:04:59 +0300

    ft:router-link - links the various components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import our components
+import{MainComponent} from './main/main.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // declaring our routes
+  {path: 'main', component: MainComponent},
+  {path: 'search', component: SearchComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  faMagnifyingGlass =faMagnifyingGlass
 
   constructor() { }
 
